dbb8299666b614c92b558bc41d4be9bfc33c9672
Child of Nature <kudusilham55@gmail.com>
2026-04-30 12:14:46 +0700

    fix port for railway deployment

--- app.py
+++ app.py
@@ -417,5 +417,8 @@ def update_booking(id, status):
 
     return redirect("/admin")
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
